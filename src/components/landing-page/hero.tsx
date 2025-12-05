9a616c31e8df685b37c80a4fdbca51a57ab3c1bc
Lekan Saheed <lknsaheed@gmail.com>
2025-12-05 12:26:15 +0100

    WIP: Landing page, trying out images on hero section

--- src/components/landing-page/hero.tsx
+++ src/components/landing-page/hero.tsx
@@ -2,23 +2,31 @@ import React from "react";
 import Button from "../button";
 import Link from "next/link";
 import { SIGNUP } from "@/constants/routes";
+import heroImage from "../../../public/hero-image.jpg";
 
 const HeroSection = () => {
   return (
-    <div className="py-[40px] px-4">
-      <div className="mb-[40px]">
+    <div
+      className="pt-[60px] px-4 pb-[120px]"
+      style={{
+        backgroundImage: `url(${heroImage.src})`,
+        backgroundSize: "cover",
+        backgroundPosition: "top",
+      }}
+    >
+      <div className="mb-[40px] text-center">
         <h1 className="uppercase font-league  text-[50px] font-bold">
           <span className=""> transform your body</span> <br />
           <span className="text-default">elevate your lifestyle.</span>
         </h1>
-        <p className="font-light text-muted-foreground">
+        <p className="font-light text-muted-foreground bg-[#000]/60 inline-block p-1 ">
           Gym training is a structured and disciplined approach to physical
           exercise that focuses on strength, endurance and overall fitness
           improvement.
         </p>
       </div>
 
-      <div>
+      <div className="flex justify-center">
         <Link href={SIGNUP}>
           <Button
             label="JOIN US NOW"
